e254fa2dea68c282bb9b2215809136cc26912355
Aleksey Brinkevich <alesha20113@mail.ru>
2021-11-01 09:02:39 +0300

    no message

--- Test1/Views/Controls/CalendarView.xaml.cs
+++ Test1/Views/Controls/CalendarView.xaml.cs
@@ -1,53 +1,130 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Globalization;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Threading.Tasks;
 
 namespace Test1.Views.Controls
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CalendarView : ContentView
     {
-        public ObservableCollection<Month> Months { get; set; }
-        public Month NextMonth { get; set; }
-        public Month PreviousMonth { get; set; }
+
+        public Calendar Calendar { get; set; }
 
         public CalendarView()
         {
-            Months = new ObservableCollection<Month>()
-            {
-                new Month(DateTime.Now.AddMonths(-1)),
-                new Month(),
-                new Month(DateTime.Now.AddMonths(1)),
-            };
+            Calendar = new Calendar();
             InitializeComponent();
-            carousel.CurrentItem = Months[1];
+            carousel.CurrentItem = Calendar.Months[1];
         }
 
-        private void CarouselView_CurrentItemChanged(object sender, CurrentItemChangedEventArgs e)
+
+        private bool locker = true;
+        private async void carousel_PositionChanged(object sender, PositionChangedEventArgs e)
         {
-            if(e.CurrentItem is Month month)
+            //if (e.CurrentPosition == 2 && e.PreviousPosition == 1)
+            //{
+            //    if (!locker)
+            //    {
+            //        locker = true;
+            //        return;
+            //    }
+            //    await Task.Run(() => Calendar.ChangeMonthToNext());
+            //}
+            //else if (e.CurrentPosition == 0 && e.PreviousPosition == 1)
+            //{
+            //    if (!locker)
+            //    {
+            //        locker = true;
+            //        return;
+            //    }
+            //    await Task.Run(() => Calendar.ChangeMonthToPrevious());
+            //    locker = false;
+            //}
+        }
+
+        private async void ButtonLeft_Clicked(object sender, EventArgs e)
+        {
+            if (carousel.Position == 1)
             {
-                MonthNameAndYear.Text = month.Date.ToString("MMMM yyyy");
-                Months.Add(new Month(month.Date.AddMonths(1)));
+                carousel.Position--;
+                await Task.Run(() => Calendar.ChangeMonthToPrevious());
             }
         }
 
-        private void CarouselView_RemainingItemsThresholdReached(object sender, EventArgs e)
+        private async void ButtonRight_Clicked(object sender, EventArgs e)
         {
-            //if (((CarouselView)sender).CurrentItem  is Month month)
-            //{
-            //    Months.Add(new Month(month.Date.AddMonths(1)));                    
-            //}
+            if (carousel.Position == 1)
+            {
+                carousel.Position++;
+                await Task.Run(() => Calendar.ChangeMonthToNext());
+            }
         }
+
+        private void DaySelected(object sender, EventArgs e)
+        {
+            Calendar.SelectedDay = (sender as Grid).BindingContext as Day;
+            //(sender as Grid).BackgroundColor = Color.LightBlue;
+        }
+
     }
 
+    public class Calendar : INotifyPropertyChanged
+    {
+        private Day selectedDay;
+        public ObservableCollection<Month> Months { get; set; }
+        public Month CourrentMonth { get; set; }
+        public ObservableCollection<Reminder> Reminders { get; set; }
+        public Day SelectedDay { 
+            get => selectedDay;
+            set
+            {
+                selectedDay = value;
+                OnPropertyChanged("SelectedDay");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Calendar()
+        {
+            Reminders = new ObservableCollection<Reminder>()
+            {
+                new Reminder("T1", "fgsgsfdgsdgf", DateTime.Now, DateTime.Now.AddMinutes(10)),
+                new Reminder("gfhddfghdf", "fgsgsfdgsdgf", DateTime.Now, DateTime.Now.AddMinutes(10)),
+                new Reminder("T2", "fgsgsfdgsdgf", DateTime.Now, DateTime.Now.AddMinutes(10)),
+                new Reminder("T3", "fgsgsfdgsdgf", DateTime.Now.AddDays(3), DateTime.Now.AddDays(3).AddMinutes(10)),
+            };
+            Months = new ObservableCollection<Month>()
+            {
+                new Month(DateTime.Now.AddMonths(-1), Reminders),
+                new Month(Reminders),
+                new Month(DateTime.Now.AddMonths(1), Reminders)
+            };
+        }
+
+        public async void ChangeMonthToNext()
+        {
+            await Task.Delay(500);
+            Months.Remove(Months.First());
+            Months.Add(new Month(Months.Last().Date.AddMonths(1), Reminders));
+        }
+        public async void ChangeMonthToPrevious()
+        {
+            Months.Remove(Months.Last());
+            await Task.Delay(500);
+            Months.Insert(0, new Month(Months.First().Date.AddMonths(-1), Reminders));
+        }
+        protected void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
 
     public class Month : INotifyPropertyChanged
     {
@@ -57,22 +134,23 @@ namespace Test1.Views.Controls
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public Month()
-            :this(DateTime.Now)
+        public Month(ObservableCollection<Reminder> reminders)
+            : this(DateTime.Now, reminders)
         {
         }
 
-        public Month(DateTime date)
+        public Month(DateTime date, ObservableCollection<Reminder> reminders)
         {
             Weeks = new ObservableCollection<Week>();
             Date = new DateTime(date.Year, date.Month, 1);
-            DateTime weekStart = Date.AddDays(-(int)DateTime.Today.DayOfWeek);
+            DateTime weekStart = Date.AddDays(-(int)Date.DayOfWeek + 1);
             for (int i = 0; i < 6; i++)
             {
                 ObservableCollection<Day> tempWeek = new ObservableCollection<Day>();
                 for (int j = 0; j < 7; j++)
                 {
-                    tempWeek.Add(new Day(weekStart.AddDays(i * 7 + j)));
+                    DateTime dateOfDay = weekStart.AddDays(i * 7 + j);
+                    tempWeek.Add(new Day(dateOfDay, new ObservableCollection<Reminder>(reminders.Where((r) => r.Beginning.Date == dateOfDay).ToList())));
                 }
                 Weeks.Add(new Week(tempWeek));
             }
@@ -102,16 +180,35 @@ namespace Test1.Views.Controls
 
     public class Day : INotifyPropertyChanged
     {
-        public ObservableCollection<Event> Events { get; set; }
+        private DateTime _date;
+        private ObservableCollection<Reminder> reminders;
+
+        public ObservableCollection<Reminder> Reminders
+        {
+            get => reminders;
+            set
+            {
+                reminders = value;
+                OnPropertyChanged("");
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        public DateTime Date { get; set; }
+        public DateTime Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                OnPropertyChanged("Date");
+            }
+        }
         public int Number { get; set; }
 
-        public Day(DateTime date)
+        public Day(DateTime date, ObservableCollection<Reminder> reminders)
         {
-            Events = new ObservableCollection<Event>();
             Date = date;
+            Reminders = reminders;
         }
         protected void OnPropertyChanged(string propName)
         {
@@ -119,11 +216,28 @@ namespace Test1.Views.Controls
         }
     }
 
-    public class Event : INotifyPropertyChanged
+    public class Reminder : INotifyPropertyChanged
     {
+        private string name;
+
+        public Reminder(string name, string discription, DateTime beginning, DateTime end)
+        {
+            Name = name;
+            Discription = discription;
+            Beginning = beginning;
+            End = end;
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public string Name { get; set; }
+        public string Name { 
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
         public string Discription { get; set; }
         public DateTime Beginning { get; set; }
         public DateTime End { get; set; }
